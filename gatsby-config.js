6a558f0bfa31ff0941e7b789ebc00ac82990bd64
SADmammoth <lo.ma200018@gmail.com>
2021-05-30 00:58:05 +0300

    init: gatsby.js project with starter gatsby-contentful-i18n

---
+++ gatsby-config.js
@@ -0,0 +1,33 @@
+const languages = require('./src/data/languages');
+
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby with Contentful`,
+    languages
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-plugin-i18n',
+      options: {
+        langKeyForNull: 'any',
+        langKeyDefault: languages.defaultLangKey,
+        useLangKeyLayout: false
+      }
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `g8yquz7s5gui`,
+        accessToken: `1_FUcrWXVbZ4LxUcLY6JT9IpMqA1X8m3mLnLCRNSLSI`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
+}
